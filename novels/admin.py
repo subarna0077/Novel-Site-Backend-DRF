874c5239ade497eb64946311712601f8ba8e2196
subh077 <88318416+subarna0077@users.noreply.github.com>
2025-08-21 18:51:13 +0545

    Registered models to the Admin
    --Registered the models to the admin panel
    -- Customized the admin panel to show inline chapters, list_display etc

--- novels/admin.py
+++ novels/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Novel, Chapter, UserProfile
+
+#Tabular inline allows u to edit the related model together on the same page with parent
+#Since, chapter is related to its parent model Novel, when we open a Novel in a admin, we can add/edit it's chapters directly under
+#novel form
+class ChapterInline(admin.TabularInline):
+    model = Chapter
+    extra = 0
+
+@admin.register(Novel)
+class NovelAdmin(admin.ModelAdmin):
+    list_display= ('title', 'author', 'is_published')
+    prepopulated_fields={"slug": ("title",)}
+    inlines = [ChapterInline]
+
+
+admin.site.register(User)
+admin.site.register(Chapter)
+admin.site.register(UserProfile)
