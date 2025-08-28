b80d333a15f8e1aac03881b73959db4e12af41c7
subh077 <88318416+subarna0077@users.noreply.github.com>
2025-08-29 00:35:13 +0545

    Added permission class to base level action: list and create

--- novels/views/comment_views.py
+++ novels/views/comment_views.py
@@ -4,12 +4,19 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from novels.models import Novel, Comment
 from novels.serializers import CommentSerializer
 from novels.permissions import IsCommentOwnerOrAuthorOrReadOnly
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 class NovelCommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsCommentOwnerOrAuthorOrReadOnly]
+
+    def get_permissions(self):
+        if self.action == 'list':
+            return [AllowAny()]
+        if self.action == 'create':
+            return [IsAuthenticated()]
+        return [IsCommentOwnerOrAuthorOrReadOnly()]
 
     def get_queryset(self):
         novel_slug = self.kwargs.get('novel_pk')  # Fixed the bug from your original code
